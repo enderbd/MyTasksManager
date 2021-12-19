71171a3d16658b9284e2f124550f2e2660bfa838
enderbd <bogdan.d@outlook.com>
2021-12-19 18:47:22 +0100

    More work on the MyTasks app. - Tasks can be added and deleted. Interface relatively beautified. TODO: increase the line limit for the texts ONLY  on the tapped task ( is it even possible? )

--- MyTasks/AddTaskView.swift
+++ MyTasks/AddTaskView.swift
@@ -9,26 +9,38 @@ import SwiftUI
 
 struct AddTaskView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.presentationMode) var presentationMode
     @State var taskTitle = ""
     var body: some View {
         VStack {
             TextField("New Task", text: $taskTitle )
-            Button(action: {
-                let newTask = MyTask(context: viewContext)
-                newTask.title = taskTitle
-                
-                do {
-                    try viewContext.save()
-                } catch {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    let nsError = error as NSError
-                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                .submitLabel(.go)
+                .onSubmit {
+                    addTask()
                 }
-
-            }) {
+            Button(action: addTask) {
                         Text("Add task to list")
-            }
+            } 
+        }
+    }
+    
+    func addTask() {
+        let newTask = MyTask(context: viewContext)
+        newTask.title = taskTitle
+        if (taskTitle.count > 20 ) {
+            newTask.isTooLong = true
+        } else {
+            newTask.isTooLong = false
+        }
+        
+        do {
+            try viewContext.save()
+            presentationMode.wrappedValue.dismiss()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
--- MyTasks/MyTasksView.swift
+++ MyTasks/MyTasksView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct MyTasksView: View {
     
+    @Environment(\.managedObjectContext) private var viewContext
+    @State var taskLineLimit = 1
+    @State var changeLineLimit = true
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \MyTask.title, ascending: true)],
         animation: .default)
@@ -18,8 +21,17 @@ struct MyTasksView: View {
         NavigationView{
             List {
                 ForEach(tasks) { grabbedTask in
-                    Text(grabbedTask.title!)
+                    //TODO: expand only the current item
+                    if (grabbedTask.isTooLong) {
+                        Text(grabbedTask.title!)
+                            .lineLimit(taskLineLimit)
+                            .onTapGesture(perform: toggleLineLimit)
+                    } else {
+                        Text(grabbedTask.title!)
+                            .lineLimit(taskLineLimit)
+                    }
                 }
+                .onDelete(perform: deleteItems)
             }
             .navigationTitle("My Tasks")
             .toolbar {
@@ -29,6 +41,23 @@ struct MyTasksView: View {
                     }
                 }
             }
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { tasks[$0] }.forEach(viewContext.delete)
+            try? viewContext.save()
+        }
+    }
+    
+    private func toggleLineLimit() {
+        if changeLineLimit {
+            taskLineLimit = 2
+            changeLineLimit = false
+        } else {
+            taskLineLimit = 1
+            changeLineLimit = true
         }
     }
 }
